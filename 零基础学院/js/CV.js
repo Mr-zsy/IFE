0e1aff09304c36850a5140cbb27f833ca90b916d
Mr-zsy <921358361@qq.com>
2018-04-26 19:59:30 +0800

    2.4

--- 零基础学院/js/CV.js
+++ 零基础学院/js/CV.js
@@ -20,8 +20,8 @@ for(var i=0;i<9;i++){
 	 	$(this).height("50px");
 	 	$(this).css({
 	 		"font-size":"30px",
-	 		"box-shadow":"1px 2px 2px #717070"
-	 		"-webkit-box-shadow":"1px 2px 2px #717070"
+	 		"box-shadow":"1px 2px 2px #717070",
+	 		"-webkit-box-shadow":"1px 2px 2px #717070",
 	 		"-moz-box-shadow":"1px 2px 2px #717070"
 	 	});
 	 }, function() {
@@ -29,8 +29,8 @@ for(var i=0;i<9;i++){
 	 	$(this).height("40px");
 	 	$(this).css({
 	 		"font-size": "25px",
-	 		"box-shadow":"none"
-	 		"-webkit-box-shadow":"none"
+	 		"box-shadow":"none",
+	 		"-webkit-box-shadow":"none",
 	 		"-moz-box-shadow":"none"
 	 	});
 	 });
